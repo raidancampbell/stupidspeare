3396bb5e8948d3e9ea7ce69011bf07ddab1f3058
R. Aidan Campbell <aidan.campbell@gmail.com>
2016-08-20 09:33:08 -0400

    !remind times can be fractions rendered with a decimal point  - code refactorings

--- stupidspeare.py
+++ stupidspeare.py
@@ -47,17 +47,18 @@ class TestBot(irc.bot.SingleServerIRCBot):
         self.do_command(event, event.arguments[0])
 
     def on_pubmsg(self, connection, e):
-        a = e.arguments[0].split(":", 1)
+        message_text = e.arguments[0]
+        a = message_text.split(":", 1)
         # if someone sent a line saying "mynick: command"
         if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
             # split an trim it to get "command"
             self.do_command(e, a[1].strip())
-        elif e.arguments[0].startswith('!'):
-            self.do_command(e, e.arguments[0].strip())
-        # CM1000K is not good at python
-        elif 'buzzfeed.com' in e.arguments[0] or 'huffingtonpost.com' in e.arguments[0]:
+        elif message_text.startswith('!'):
+            self.do_command(e, message_text.strip())
+        # hiss at buzzfeed/huffpost, characters greater than 128, and on the word 'moist'
+        if 'buzzfeed.com' in message_text or 'huffingtonpost.com' in message_text:
             connection.privmsg(e.target, 'hisss fuck off with your huffpost buzzfeed crap')
-        if not all(ord(c) < 128 for c in e.arguments[0]) or 'moist' in e.arguments[0]:
+        elif not all(ord(c) < 128 for c in e.arguments[0]) or 'moist' in message_text:
             connection.privmsg(e.target, 'hisss')
         print('PUBMSG: ' + e.arguments[0])
 
@@ -67,12 +68,13 @@ class TestBot(irc.bot.SingleServerIRCBot):
         if cmd_text == "leave" or cmd_text == "!leave":
             self.disconnect()
         elif cmd_text == "die" or cmd_text == "!die":
-            connection.privmsg(event.target, event.source.nick + ': ' + "function disabled until owner privs are implemented")
+            connection.privmsg(event.target, event.source.nick + ': ' + "disabled until owner privs are implemented")
             # self.die()
         elif cmd_text == "ping" or cmd_text == "!ping":
             connection.privmsg(event.target, event.source.nick + ': ' + "Pong!")
         elif cmd_text == "source" or cmd_text == "!source":
-            connection.privmsg(event.target, event.source.nick + ': ' + "https://github.com/raidancampbell/stupidspeare")
+            connection.privmsg(event.target,
+                               event.source.nick + ': ' + "https://github.com/raidancampbell/stupidspeare")
         elif cmd_text.startswith("remind") or cmd_text.startswith("!remind"):
             wait_time, reminder_text = self.parse_remind(cmd_text)
             kwargs = {'wait_time_s': wait_time, 'reminder_text': reminder_text, 'connection': connection,
@@ -81,7 +83,7 @@ class TestBot(irc.bot.SingleServerIRCBot):
                 threading.Thread(target=TestBot.wait_then_remind_to, kwargs=kwargs).start()
             else:
                 connection.privmsg(event.target, event.source.nick + ': ' +
-                          'Usage is "!remind [in] 5 (second[s]/minute[s]/hour[s]/day[s]) reminder text"')
+                                   'Usage is "!remind [in] 5 (second[s]/minute[s]/hour[s]/day[s]) reminder text"')
         else:
             pass  # not understood command
 
@@ -98,26 +100,27 @@ class TestBot(irc.bot.SingleServerIRCBot):
             reminder_text = text[text.index('remind random') + len('remind random'):]
         else:
             for word in text.split(' '):
-                if word.isnumeric():  # warning: this can pass through '1.2', which will throw an error on int('1.2')
+                if word.isnumeric():  # we parse it into a float now, and round it at the end
                     try:  # grab the time
-                        wait_time = int(word)
-                    except ValueError:  # so we catch that if it happens, and round it back into being reasonable
-                        wait_time = int(round(float(word)))
+                        wait_time = float(word)
+                    except ValueError:
+                        print('ERR: failed to parse: ' + word + ' into a float!')
+                        return 0, ''
                 elif wait_time and not finished_parsing:  # we grabbed the time, but need the units
-                    if word.lower() == 'min' or word.lower() == 'mins' or word.lower() == 'minute' or word.lower() == 'minutes':
+                    if word.lower() in ['min', 'mins', 'minute', 'minutes']:
                         wait_time *= 60
-                    elif word.lower() == 'hr' or word.lower() == 'hrs' or word.lower() == 'hours' or word.lower() == 'hour':
+                    elif word.lower() in ['hr', 'hrs', 'hours', 'hour']:
                         wait_time *= 60 * 60
-                    elif word.lower() == 'day' or word.lower() == 'days':
+                    elif word.lower() in ['day', 'days']:
                         wait_time = wait_time * 24 * 60 * 60
                     finished_parsing = True
                 elif finished_parsing:
                     reminder_text += word + ' '
-        return wait_time, reminder_text.strip()
+        return int(round(wait_time)), reminder_text.strip()  # round the time back from a float into an int
 
     @staticmethod
     def wait_then_remind_to(**kwargs):
-        print('>>reminding about ' + kwargs['reminder_text'] + ' in ' + str(kwargs['wait_time_s']//60) + ' minutes')
+        print('>>reminding about ' + kwargs['reminder_text'] + ' in ' + str(kwargs['wait_time_s'] // 60) + ' minutes')
         kwargs['connection'].privmsg(kwargs['channel'], "Okay, I'll remind you about " + kwargs['reminder_text'])
         time.sleep(kwargs['wait_time_s'])
         kwargs['connection'].privmsg(kwargs['channel'], kwargs['nick'] + ': ' + kwargs['reminder_text'])
@@ -128,8 +131,10 @@ def parse_args():
     parser.add_argument('--server', type=str, help="Server address", required=True)
     parser.add_argument('--port', type=int, help="Server port", required=True)
     parser.add_argument('--botnick', type=str, help="Nick to use for bot", required=True)
-    parser.add_argument('--channel', type=str, help='Channels to join on connect (#chan1[,#chan2,#chan3])', required=True)
+    parser.add_argument('--channel', type=str, help='Channels to join on connect (#chan1[,#chan2,#chan3])',
+                        required=True)
     return parser.parse_args()
+
 
 # Execution begins here, if called via python interpreter
 if __name__ == '__main__':
